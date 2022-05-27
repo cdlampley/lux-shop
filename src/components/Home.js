82391dadb5ddd035c294d246e70e4d027e06f236
cdlampley <cdlampley@gmail.com>
2022-05-27 18:28:24 -0400

    Updated files

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Home() {
+
+    return (
+        <div>
+            <div class="card bg-dark text-white">
+                <img src="./img/woman-3040029_1920.jpg" class="card-img" alt="woman shopping" />
+                <div class="card-img-overlay">
+                    <h5 class="card-title">Lux Shop</h5>
+                    <p class="card-text">Check out the latest trends.</p>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Home;
