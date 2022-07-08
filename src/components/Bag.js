1a5ff8086d2cc54185c6a46443b5550a15b06e6f
cdlampley <cdlampley@gmail.com>
2022-07-08 11:11:15 -0400

    updated Bag.js

--- src/components/Bag.js
+++ src/components/Bag.js
@@ -31,31 +31,6 @@ const Bag = ({
                             ref={refContainer}
                             onClick={(e) => e.stopPropagation()}>
                             <h2 className="display-6 fw-bolder text-center">Shopping Bag</h2>
-                            <div>
-                                {bag.map((item) => (
-                                    <div
-                                        key={item.id}>
-                                        <img
-                                            src={item.img}
-                                            alt={item.name}
-                                        />
-                                        <p>
-                                            {item.amount}
-                                        </p>
-                                        <h3>
-                                            {item.name}
-                                        </h3>
-                                        <div>
-                                            <button onClick={() => delItem(item.id)}></button>
-                                            <p>
-                                                {Dollar.format(item.price)}
-                                            </p>
-                                            <button onClick={() => addItem(item)}></button>
-                                        </div>
-                                    </div>
-                                ))}
-                                {bag.length > 0 && <p>Total: {Dollar.format(total(bag))}</p>}
-                            </div>
                         </div>
                     </div>
                 </div>
